40833ffde26c6e0a7951790b1bac82d0710beadc
Nate Grigg <github@nategrigg.com>
2022-12-25 18:07:04 -0700

    loop on input

--- screen_size.py
+++ screen_size.py
@@ -12,8 +12,12 @@ def width_from(diag, aspect_ratio=widescreen):
     return aspect_ratio * height_from(diag, aspect_ratio)
 
 if __name__ == '__main__':
-    diag_in = float(input('TV size (inches): '))
+  s = input('TV size (inches): ')
+  while s:
+    diag_in = float(s)
     height_in = height_from(diag_in)
     width_in = width_from(diag_in)
 
     print(f'A {diag_in:.0f}-inch TV is about {in_to_cm(height_in):.1f}cm x {in_to_cm(width_in):.1f}cm')
+    s = input('TV size (inches): ')
+

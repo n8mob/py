698fad954c8dc6aa4cc3d0f14c9e5ad173416a47
Nate Grigg <github@nategrigg.com>
2023-03-28 15:44:08 -0600

    draft 1

--- unscramble.py
+++ unscramble.py
@@ -1,4 +1,27 @@
-def main:
-    scrambled = input('type scrambled: ')
-    
+import random
+
+def is_good(can):
+  return (
+    '  ' not in can and
+    not can[0] == ' ' and
+    not can[-1] == ' '
+    )
+
+def main():
+  orig = input('type scrambled: ')
+  working = list(orig)
   
+  i = 0
+    
+  while i < 20:
+    random.shuffle(working)
+    can = ''.join(working)
+    if is_good(can):
+      i+=1
+      print(can)
+    else:
+      continue
+    
+    
+if __name__ == '__main__':
+  main()

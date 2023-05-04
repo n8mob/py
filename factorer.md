c398c6c7c0cc78a297c3293b3089d2cf9abc3987
Nate Grigg <ngrigg@ancestry.com>
2023-05-04 16:58:19 -0600

    still in progress

---
+++ factorer.md
@@ -0,0 +1,24 @@
+
+| member                       | description                                                             |
+|------------------------------|-------------------------------------------------------------------------|
+| integers                     | just integers                                                           |
+| primes                       | a list of primes                                                        |
+| sqrt(n)                      | the square root of each integer                                         |
+| max factor                   | the next integer higher than the square root                            |
+| index of max prime factor    | the index of the prime next prime number higher than the square root    |
+
+# Matrix of Examples
+
+## Reference Primes
+| index | 0 | 1 | 2 | 3 | 4  | 5  | 6  | 7  | 8  | 9  | 10 | 11 | 12 | 13 |
+|-------|---|---|---|---|----|----|----|----|----|----|----|----|----|----|
+| Prime | 2 | 3 | 5 | 7 | 11 | 13 | 17 | 19 | 23 | 29 | 31 | 33 | 37 | 41 |
+
+## Integers to Factor
+| index         | 0  | 1 | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   | 10  | 11  | 12  | 13  | 14  |
+|---------------|----|---|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|-----|
+| int           | 0  | 1 | 2   | 3   | 4   | 5   | 6   | 7   | 8   | 9   | 10  | 11  | 12  | 13  | 14  |
+| sqrt          | 0  | 1 | 1.4 | 1.7 | 2   | 2.2 | 2.4 | 2.6 | 2.8 | 3   | 3.1 | 3.3 | 3.4 | 3.6 | 3.7 |    
+| ceiling       | 1  | 1 | 2   | 2   | 2   | 3   | 3   | 3   | 3   | 3   | 4   | 4   | 4   | 4   | 4   |
+| next prime    | 2  | 2 | 2   | 2   | 2   | 3   | 3   | 3   | 3   | 3   | 5   | 5   | 5   | 5   | 5   |
+| prime's index | 0  | 0 | 0   | 0   | 0   | 1   | 1   | 1   | 1   | 1   | 2   | 2   | 2   | 2   | 2   |
--- factorer.py
+++ factorer.py
@@ -3,29 +3,43 @@ from unittest import TestCase
 
 
 class Factorer:
+    """Finding Prime Factorizations
+
+    """
     def __init__(self, n):
         self.n = n
-        self.factors = {n: {} for n in range(2, n+1)}
-        self.primes = [2, 3]
-        self.max_factor = [math.ceil(math.sqrt(m)) for m in range(n)]
+        n_range = range(1, n+1)
 
-        if n < 3:
-            return
-        else:
-            self.compute_factors()
+        self.factors = {n: {} for n in n_range}
+        self.primes = [2, 3]
+        self.max_factor = {m: math.ceil(math.sqrt(m)) for m in self.factors.keys()}
+        self.index_of_max_prime_factor = {m: -1 for m in self.factors.keys()}
+
+        for i_p in range(len(self.primes)):
+            for i_n in range(len(self.max_factor)):
+                if self.primes[i_p] <= self.max_factor[i_n]:
+                    self.index_of_max_prime_factor[i_n] = i_p
+                else:
+                    self.index_of_max_prime_factor[i_n] = -1
 
     def compute_factors(self):
         for m, factors in self.factors.items():
             remainder = m
-            for f in self.primes[self.index_of_prime_ceil[m]]:
-                if remainder % f == 0:
-                    factors[f] = factors[f] + 1 if f in factors else 1
-                    remainder = remainder // f
-            if remainder > 0:
 
+            for index_of_prime in range(0, self.index_of_max_prime_factor[m]):
+                prime_factor = self.primes[index_of_prime]
+                if remainder % prime_factor == 0:
+                    factors[prime_factor] = factors[prime_factor] + 1 if prime_factor in factors else 1
+                    remainder = remainder // prime_factor
+            if remainder == 0:  # factoring complete
+                if not factors:  # new prime?
+                    self.primes.append(m)
+                    break
 
     def index_of_max_prime_factor(self, m):
-        if self.max_factor[m] > self.primes[-1]:
+        for i, p in enumerate(self.primes):
+            if p >= m:
+                return i
 
     @staticmethod
     def check_factorization(m, factors: {int: int}):
@@ -36,7 +50,7 @@ class Factorer:
         return math.prod([f ** e for f, e in factors.items()])
 
 
-class TestFactorer(TestCase):
+class TestHelperFunctions(TestCase):
     def test_calculate_6(self):
         self.assertEqual(6, Factorer.calculate({2: 1, 3: 1}))
 
@@ -75,3 +89,38 @@ class TestFactorer(TestCase):
 
     def test_check_36(self):
         self.assertTrue(Factorer.check_factorization(36, {2: 2, 3: 2}))
+
+
+class TestInit(TestCase):
+    def test_init_factors_dictionary_with_2(self):
+        f = Factorer(2)
+        self.assertEqual(2, f.n)
+        self.assertIsNotNone(f.factors)
+        self.assertEqual(2, len(f.factors))
+        self.assertIn(1, f.factors)
+        self.assertIn(2, f.factors)
+
+    def test_init_max_factors_with_2(self):
+        f = Factorer(2)
+        self.assertEqual([1, 2], f.max_factor)
+
+    def test_init_max_factors_with_14(self):
+        f = Factorer(14)
+        self.assertEqual([1, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4], f.max_factor)
+
+    def test_init_empty_prime_indices(self):
+        f = Factorer(14)
+        self.assertEqual(14, len(f.index_of_max_prime_factor))
+
+
+class TestCompute(TestCase):
+    def test_compute_3(self):
+        f = Factorer(3)
+
+        f.compute_factors()
+
+        self.assertEqual([1, 2, 3], list(f.factors.keys()))
+
+        for n, factors in f.factors.items():
+            self.assertIsNotNone(factors, f"{n}: factors should be a dictionary")
+            self.assertGreater(0, len(factors), f"{n}: factors should not be empty")

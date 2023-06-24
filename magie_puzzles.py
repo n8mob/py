45865e9bb5b479c275b82715471a3834dfc65f16
Nate Grigg <n8mob@nategrigg.com>
2023-06-23 22:46:40 -0600

    incorrect, try again

--- magie_puzzles.py
+++ magie_puzzles.py
@@ -14,22 +14,21 @@ class CategoryMenu:
             print(line)
 
 
-def choose_from_dict(choices):
+def choose_from_dict(choices, prompt):
     choice_menu = list(choices.keys())
     for i, choice_name in enumerate(choice_menu):
         print(f"{i}: {choice_name}")
-    choice = input('which category? ')
+    choice = input(prompt + ' ')
     if str.isnumeric(choice):
         return choices[choice_menu[int(choice)]]
     else:
         return choices[choice]
 
+def choose_from_list(choices, prompt):
+    for i, choice_name in enumerate(choices):
+        print(f'\n{i}: {choice_name}')
 
-def choose_index(choices):
-    for i, choice in enumerate(choices):
-        print(f"{i}: {choice}")
-
-    choice = input('? ')
+    choice = input(prompt + ' ')
     return int(choice)
 
 
@@ -37,14 +36,22 @@ if __name__ == '__main__':
     url = 'https://puzzles.magiegame.com/menus/'
     response = requests.get(url)
     category_menu = CategoryMenu(response.json()[0])
-    chosen_category = choose_from_dict(category_menu.categories_by_name)
+    chosen_category = choose_from_dict(category_menu.categories_by_name, 'which category?')
 
     level_display_names = ['\n'.join(level['levelName']) for level in chosen_category]
-    level_index = choose_index(level_display_names)
-    print('\nlevel:\n' + level_display_names[level_index] + '\n')
+    level_index = choose_from_list(level_display_names, 'which level?')
 
     chosen_level = chosen_category[level_index]
 
     for puzzle in chosen_level['puzzles']:
+        print()
         CategoryMenu.print(puzzle['clue'])
+
         guess = input('\n')
+
+        while guess != puzzle['winText']:
+            print('INCORRECT')
+            print('TRY AGAIN')
+            guess = input()
+
+        print('CORRECT\n')

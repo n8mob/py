26ff1a0ce3bb56f10be94f4a1e1cfe3eeb8a99ad
Nate Grigg <n8mob@nategrigg.com>
2023-07-16 17:33:28 -0600

    show correct letters and celebrate with blinkquit

--- magie_puzzles.py
+++ magie_puzzles.py
@@ -1,7 +1,8 @@
 import sys
+import curses
+import time
 
 import requests
-
 
 class CategoryMenu:
     def __init__(self, menu):
@@ -34,12 +35,11 @@ def choose_from_list(choices, prompt):
     return int(choice)
 
 
-if __name__ == '__main__':
+def try_a_puzzle():
     url = 'https://puzzles.magiegame.com/menus/'
     response = requests.get(url)
     category_menu = CategoryMenu(response.json()[0])
     chosen_category = choose_from_dict(category_menu.categories_by_name, 'which category?')
-
     for level in chosen_category:
         for puzzle in level['puzzles']:
             print()
@@ -47,12 +47,57 @@ if __name__ == '__main__':
             print()
             CategoryMenu.print(puzzle['init'])
 
-            guessChar = sys.stdin.read(1)
+            guessPhrase = None
+            guessChar = None
+            win_index = 0
+            winChar = puzzle['winText'][win_index]
 
-            for i, winChar in enumerate(puzzle['winText']):
-                while guessChar != winChar:
-                    guessChar = sys.stdin.read(1)
-                print(f'\r{puzzle["winText"][:i+1]}', end='')
-                guessChar = ''
+            while guessPhrase != puzzle['winText']:
+                guessChar = getch()
+                if guessChar == winChar:
+                    print(puzzle['winText'][:win_index])
+                else:
+                    print('\b')
 
             print('\nCORRECT')
+
+
+def guess_loop(scr: curses.window):
+    curses.start_color()
+
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+
+    incorrect = curses.color_pair(1) | curses.A_BOLD
+    correct = curses.color_pair(2) | curses.A_BOLD
+
+    win_message = 'test'
+    guess_message = ''
+    i = 0
+    guess_char = 'X'
+
+    while guess_message != win_message:
+        scr.refresh()
+        if guess_char == win_message[i]:
+            i += 1
+            guess_message += guess_char
+
+            scr.addstr(2, 2, guess_message, correct)
+        else:
+            scr.addstr(2, 2, guess_message, correct)
+            scr.addstr(guess_char, incorrect)
+
+        guess_char = chr(scr.getch())
+
+    a = True
+
+    for blinkquit in range(12):
+        time.sleep(0.6)
+        color = correct if a else incorrect
+        a = not a
+        scr.clear()
+        scr.addstr(2, 2, 'CORRECT', color)
+        scr.refresh()
+
+if __name__ == '__main__':
+    curses.wrapper(guess_loop)

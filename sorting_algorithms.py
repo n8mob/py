4267e6c537f7fc31a00cedbd8b6431033598bc93
Nate Grigg <ngrigg@ancestry.com>
2023-08-22 17:50:48 -0600

    test merge sort (I think)

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,30 +1,38 @@
+import math
 import random
 import unittest
 from datetime import datetime, timedelta
+from math import floor
 
-DEBUG = False
+DEBUG = True
 
 
 class TestPass:
-    def __init__(self, number, start_time, end_time=None, swaps=0):
+    def __init__(self, number, start_time, end_time=None, details=None):
         self.number = number
         self.start_time = start_time
         self.end_time = end_time
-        self.swaps = swaps
+        self.details = details or {}
 
     def __str__(self):
-        return f"| {self.number} | {self.get_elapsed()} | {self.swaps} |"
+        return f"| {self.number} | {self.get_elapsed()} | {self.get_primary_detail()} |"
 
-    def get_elapsed(self):
+    def get_elapsed(self) -> timedelta:
         return self.end_time - self.start_time
+
+    def get_primary_detail(self):
+        if 'swaps' in self.details:
+            return {'swaps': self.details['swaps']}
+        else:
+            for detail, value in self.details.items():
+                if value is not None:
+                    return {detail: value}
 
 
 class SortingTestCase:
-    def __init__(self, test_data, expected_elapsed, expected_passes, expected_swaps, int_size=4):
+    def __init__(self, test_data, expected_details: dict = None, int_size=4):
         self.test_data = self.make_ints(test_data, int_size)
-        self.expected_elapsed = expected_elapsed
-        self.expected_passes = expected_passes
-        self.expected_swaps = expected_swaps
+        self.expected_details = expected_details or {}
         self.test_passes = []
 
     @staticmethod
@@ -38,9 +46,8 @@ class SortingTestCase:
 
 
 class TestSorting(unittest.TestCase):
-    @staticmethod
-    def get_random_bytes():
-        return [
+    def setUp(self) -> None:
+        self.test_arrays = [
             random.randbytes(1_000),
             random.randbytes(2_000),
             random.randbytes(4_000),
@@ -53,92 +60,164 @@ class TestSorting(unittest.TestCase):
             random.randbytes(512_000)
         ]
 
-    def setUp(self) -> None:
-        self.test_arrays = TestSorting.get_random_bytes()
-
     def test_naive_sort(self):
-        """implementing bubble sort from memory
-        maybe it's actually something else, so I named the test "naive" sort
-        """
         test_cases = [
-            SortingTestCase(self.test_arrays[0], 125, 250, 30_000),
-            SortingTestCase(self.test_arrays[1], 250, 500, 100_000),
-            SortingTestCase(self.test_arrays[2], 500, 1000, 500_000),
-            SortingTestCase(self.test_arrays[3], 1000, 2000, 2_000_000),
-            SortingTestCase(self.test_arrays[4], 2000, 4000, 8_000_000)
+            SortingTestCase(self.test_arrays[0], {
+                'total_elapsed': timedelta(milliseconds=125),
+                'pass_elapsed': timedelta(milliseconds=150),
+                'passes': 251,
+                'swaps': 20_000}),
+            SortingTestCase(self.test_arrays[1], {
+                'total_elapsed': timedelta(milliseconds=250),
+                'pass_elapsed': timedelta(milliseconds=300),
+                'passes': 501,
+                'swaps': 2_000_000}),
+            SortingTestCase(self.test_arrays[2], {
+                'total_elapsed': timedelta(milliseconds=500),
+                'pass_elapsed': timedelta(milliseconds=500),
+                'passes': 1001,
+                'swaps': 4_000_000})
         ]
 
-        self.test_algorithm(self.naive_sort, test_cases)
+        def keep_testing(test_case: SortingTestCase, previous: TestPass):
+            if previous.number > test_case.expected_details['passes']:
+                return False
+            if 'swaps' in previous.details:
+                return previous.details['swaps'] > 0
+            return True
+
+        self.algorithm_test_helper(self.naive_sort, test_cases, keep_testing)
 
     def test_merge_sort(self):
-        """Gross - now I have to implement merge sort???"""
         test_cases = [
-            SortingTestCase(self.test_arrays[0], 25, 4, 100),
-            SortingTestCase(self.test_arrays[1], 50, 10, 1_000),
-            SortingTestCase(self.test_arrays[2], 100, 100, 2_000),
-            SortingTestCase(self.test_arrays[3], 200, 150, 4_000),
-            SortingTestCase(self.test_arrays[4], 400, 200, 8_000)
+            SortingTestCase(self.test_arrays[6], {'total_elapsed': timedelta(milliseconds=500)}),
+            SortingTestCase(self.test_arrays[7], {'total_elapsed': timedelta(milliseconds=1000)}),
+            SortingTestCase(self.test_arrays[8], {'total_elapsed': timedelta(milliseconds=2000)}),
+            SortingTestCase(self.test_arrays[9], {'total_elapsed': timedelta(milliseconds=5000)})
         ]
 
-        self.test_algorithm(self.merge_sort, test_cases)
+        self.algorithm_test_helper(self.merge_sort, test_cases, lambda test_case, previous: previous.number < 2)
 
-    def test_algorithm(self, method, test_cases):
+    def algorithm_test_helper(self, method, test_cases, keep_testing):
         for test_case in test_cases:
             with self.subTest(f"{len(test_case.test_data)} integers"):
                 start = datetime.now()
 
-                sort_passes = [TestPass(-1, datetime.now(), swaps=1)]
+                sort_passes = [TestPass(-1, datetime.now(), {'swaps': 1})]
                 sort_passes[0].end_time = sort_passes[0].start_time
 
-                while sort_passes[-1].swaps > 0:
+                while keep_testing(test_case, sort_passes[-1]):
                     sort_pass = TestPass(sort_passes[-1].number + 1, datetime.now())
 
                     method(sort_pass, test_case)
 
                     sort_pass.end_time = datetime.now()
 
-                    self.assertLessEqual(sort_pass.get_elapsed(), timedelta(milliseconds=100))
+                    if 'pass_elapsed' in test_case.expected_details:
+                        expected = test_case.expected_details['pass_elapsed']
+                        actual = sort_pass.get_elapsed()
+                        self.assertLessEqual(actual, expected, msg=f"test pass {sort_pass.number} took too long")
                     sort_passes.append(sort_pass)
 
-                total_elapsed = datetime.now() - start
-
                 elapsed_sum = timedelta(milliseconds=0)
-                total_swaps = 0
+                details = {'total_elapsed': (datetime.now() - start)}
 
                 if DEBUG:
-                    print('| iteration | start time | end time | elapsed | swaps | ')
-                    for p in sort_passes:
-                        elapsed_sum += p.get_elapsed()
-                        total_swaps += p.swaps
+                    print('| iteration | start time | end time | details | ')
+
+                for p in sort_passes:
+                    elapsed_sum += p.get_elapsed()
+                    for detail in p.details:
+                        if detail not in details:
+                            details[detail] = 1
+                        details[detail] += p.details[detail]
+                    if DEBUG:
                         print(p)
 
-                self.assertLessEqual(elapsed_sum, total_elapsed)
+                self.assertLessEqual(elapsed_sum, datetime.now() - start)
 
-                self.assertLessEqual(total_elapsed, timedelta(milliseconds=test_case.expected_elapsed), 'elapsed')
-                self.assertLessEqual(len(sort_passes), test_case.expected_passes, 'passes')
-                self.assertLessEqual(sum((p.swaps for p in sort_passes)), test_case.expected_swaps, 'swaps')
+                for detail in test_case.expected_details:
+                    if detail in details:
+                        self.assertLessEqual(
+                            details[detail],
+                            test_case.expected_details[detail],
+                            f'{detail} not less than or equal to expectation'
+                        )
 
     @staticmethod
-    def naive_sort(sort_pass, test_case):
+    def naive_sort(sort_pass: TestPass, test_case):
         for i in range(len(test_case.test_data) - 1):
-            if test_case.test_data[i] < test_case.test_data[i + 1]:
+            if test_case.test_data[i] > test_case.test_data[i + 1]:
                 tmp = test_case.test_data[i]
                 test_case.test_data[i] = test_case.test_data[i + 1]
                 test_case.test_data[i + 1] = tmp
-                sort_pass.swaps += 1
+                if 'swaps' not in sort_pass.details:
+                    sort_pass.details['swaps'] = 1
+                else:
+                    sort_pass.details['swaps'] += 1
+
+    def merge_sort(self, sort_pass, test_case):
+        self.merge_sort_segment(test_case.test_data)
+
+    def merge_sort_segment(self, a):
+        if len(a) < 2:
+            return a
+
+        p = floor(len(a) / 2)
+
+        return self.merge(self.merge_sort_segment(a[:p]), self.merge_sort_segment(a[p:]))
 
     @staticmethod
-    def merge_sort(sort_pass, test_case):
-        pass
+    def merge(a, b):
+        if not a and not b:
+            return []
+        elif not a:
+            return b
+        elif not b:
+            return a
 
-    def merge_sort_segment(self, a, b):
-        a_a, a_b = self.partition(a)
-        b_a, b_b = self.partition(b)
+        merged = []
 
+        while a and b:
+            if a[0] <= b[0]:
+                merged.append(a.pop(0))
+            else:
+                merged.append(b.pop(0))
 
+        if b:
+            return merged + b
+        else:
+            return merged + a
 
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_merge_nones(self):
+        self.assertEqual([], self.merge(None, None))
+        self.assertEqual([], self.merge([], None))
+        self.assertEqual([], self.merge(None, []))
+        self.assertEqual([], self.merge([], []))
+        self.assertEqual([1], self.merge([1], None))
+        self.assertEqual([1], self.merge(None, [1]))
+
+    def test_merge_0to1(self):
+        self.assertEqual([1], self.merge([1], []))
+        self.assertEqual([1], self.merge([], [1]))
+        self.assertEqual([1, 1], self.merge([1], [1]))
+        self.assertEqual([1, 2], self.merge([1], [2]))
+        self.assertEqual([1, 2], self.merge([2], [1]))
+
+    def test_merge_1to2(self):
+        self.assertEqual([1, 1, 2], self.merge([1], [1, 2]))
+        self.assertEqual([1, 2, 3], self.merge([1], [2, 3]))
+        self.assertEqual([1, 2, 3], self.merge([1, 2], [3]))
+        self.assertEqual([1, 2, 3], self.merge([1, 3], [2]))
+        self.assertEqual([1, 2, 3, 4], self.merge([1, 2], [3, 4]))
+        self.assertEqual([1, 2, 3, 4], self.merge([1, 3], [2, 4]))
+
+    def test_length_differs_by_2(self):
+        self.assertEqual([1, 2, 3, 4, 5, 6], self.merge([1, 2], [3, 4, 5, 6]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], self.merge([1, 2, 3, 4], [5, 6]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], self.merge([1, 4], [2, 3, 5, 6]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], self.merge([1, 6], [2, 3, 4, 5]))
+        self.assertEqual([1, 2, 3, 4, 5, 6], self.merge([2, 3, 4, 5], [1, 6]))
 
 
 if __name__ == '__main__':

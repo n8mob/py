7a85540e641d7eb27b2010d698154a110ee6377a
Nate Grigg <github@nategrigg.com>
2024-02-13 13:29:30 -0700

    z

---
+++ game_theory.py
@@ -0,0 +1,18 @@
+import random
+
+def give_and_take(
+  start_pop=10,
+  hawk_ratio=0,
+  days=100
+  ):
+  hawk_count = math.floor(start_pop*hawk_ratio)
+  dove_count = math.ceil(start_pop - hawk_count)
+  hawks = ['h' for h in range(hawk_count]
+  doves = ['d' for d in range(start_pop)
+  while days > 0:
+    for dove in doves:
+      
+  
+
+if __name__ == '__main__':
+  print('hello, Thomas')
--- web_words.py
+++ web_words.py
@@ -2,13 +2,14 @@ import requests
 
 
 class WebDictionary:
-  def __init__(self, url='https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'):
+  def __init__(self, url='https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt', debug=False):
     self.url = url
+    self.debug = debug
     self.by_length = {}
 
     response = requests.get(url)
     if not response.ok:
-      print(response.text)
+      self.printError(response.text)
       return
 
     for word in response.text.split('\n'):
@@ -17,11 +18,14 @@ class WebDictionary:
       wl = len(word)
 
       if wl not in self.by_length:
-        print(f'first word of length {wl}: {word}')
+        self.printDebug(f'first word of length {wl}: {word}')
         self.by_length[wl] = set()
 
       self.by_length[wl].add(word)
 
+  def printDebug(self, s):
+    if self.debug:
+      print(s)
 
 if __name__ == '__main__':
   d = WebDictionary()

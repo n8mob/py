448bf950967688bdfb8a5405c46bd6816784e1d1
Nate Grigg <n8mob@nategrigg.com>
2025-02-27 10:45:47 -0700

    format

--- pdfinspect/utility.py
+++ pdfinspect/utility.py
@@ -3,13 +3,13 @@ import sqlite3
 
 
 def get_db_path(pdf_path, db_path=None):
-    if not db_path:
-        db_path = os.path.dirname(pdf_path)
-    if os.path.isdir(db_path):
-        pdf_filename = os.path.basename(pdf_path)
-        db_filename = os.path.splitext(pdf_filename)[0] + '.db'
-        db_path = os.path.join(db_path, db_filename)
-    return db_path
+  if not db_path:
+    db_path = os.path.dirname(pdf_path)
+  if os.path.isdir(db_path):
+    pdf_filename = os.path.basename(pdf_path)
+    db_filename = os.path.splitext(pdf_filename)[0] + '.db'
+    db_path = os.path.join(db_path, db_filename)
+  return db_path
 
 
 def fetch_data(db_path):

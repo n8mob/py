2a7d9c2c7fbb64c986f704a1909fbd5594667b5c
Nate Grigg <n8mob@nategrigg.com>
2025-03-20 16:49:01 -0600

    count n-grams

---
+++ n_grammer.py
@@ -0,0 +1,58 @@
+import unittest
+
+
+class NGrammer:
+  def __init__(self, n):
+    self.n = n
+
+  def count(self, text) -> dict[str, int]:
+    ngrams = {}
+    for i in range(len(text) - self.n + 1):
+      ngram = text[i:i + self.n]
+      if ngram in ngrams:
+        ngrams[ngram] += 1
+      else:
+        ngrams[ngram] = 1
+
+    return ngrams
+
+
+class TestNGrammer(unittest.TestCase):
+  def setUp(self):
+    self.uut = NGrammer(2)
+
+  def test_2gram_of_ab_is_1(self):
+    text = "ab"
+    expected = {'ab': 1}
+    actual = self.uut.count(text)
+    self.assertEqual(expected, actual)
+
+  def test_2gram_of_abc_is_2(self):
+    text = "abc"
+    expected = {'ab': 1, 'bc': 1}
+    actual = self.uut.count(text)
+    self.assertEqual(expected, actual)
+
+  def test_2gram_of_abcabc_is_221(self):
+    text = "abcabc"
+    expected = {'ab': 2, 'bc': 2, 'ca': 1}
+    actual = self.uut.count(text)
+    self.assertEqual(expected, actual)
+
+  def test_3gram_of_abcabc_is_211(self):
+    self.uut = NGrammer(3)
+    text = "abcabc"
+    expected = {'abc': 2, 'bca': 1, 'cab': 1}
+    actual = self.uut.count(text)
+    self.assertEqual(expected, actual)
+
+  def test_3gram_of_ab_is_empty(self):
+    self.uut = NGrammer(3)
+    text = "ab"
+    expected = {}
+    actual = self.uut.count(text)
+    self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+  unittest.main()
